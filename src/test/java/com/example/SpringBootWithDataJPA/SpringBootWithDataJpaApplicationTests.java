35d3f7f90175ccb9df26353b7014dcc1ef26c776
gabriel-francelino <90209521+gabriel-francelino@users.noreply.github.com>
2023-02-03 11:52:15 -0300

    Enviando arquivos para o github.

---
+++ src/test/java/com/example/SpringBootWithDataJPA/SpringBootWithDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootWithDataJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
